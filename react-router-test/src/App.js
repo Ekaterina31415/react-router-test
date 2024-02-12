84a7cbd2a4ccc5186264ac1fb553bc544a669222
Ekaterina Kilina <kilina2018@bk.ru>
2024-02-12 14:55:42 +0600

    add BrowserRouter element and some example pages

--- react-router-test/src/App.js
+++ react-router-test/src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PageOne, PageTwo } from './Components/Pages';
+import { MainPage } from './Components/MainPage';
 
 function App() {
   return (
-    <div className="App">
-      hi from app.js
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} >
+            <Route index element={<div>No page is selected.</div> } />
+            <Route path="one" element={<PageOne />} />
+            <Route path="two" element={<PageTwo />} />
+          </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ react-router-test/src/Components/MainPage.jsx
@@ -0,0 +1,18 @@
+import { Link, Outlet } from 'react-router-dom';
+
+export const MainPage = () => (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/one'>Page One</Link>
+          </li>
+          <li>
+            <Link to='/two'>Page Two</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet />
+    </>
+);
